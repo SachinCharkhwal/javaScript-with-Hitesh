a0c270e145f2252c1c21054facb48c85cda90322
SachinCharkhwal <sachinjangra1898@gmail.com>
2024-01-05 15:52:07 +0530

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+// const accountId = 12345;
+// let accountEmail = 'sachin@gmail.com';
+// var accountPassword = '12345';
+// accountCity = 'jaipur';
+
+// accountPassword = 55;
+// var sachin = 5;                    // consider it as undefined 
+
+// const accountId = 2;   // not allowed ( redefine a const variable not allowed )
+ 
+let hello = function(){
+    var name = 'sachin'
+}
+
+console.log().hello();
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+// Types of variable :- 
+// --------------------
+// 01) number = 2 to power of 52-53
+// 02) BigInt    = for a vary long value ( trading king of think )
+// 03) string    = " "
+// 04) Boolean   = true/false
+// 05) null      = standalone value -(null)
+// 06) undefined =           (undefined,empty)
+// 07) Symbol    = unique (eg.figma etc. )
+
+// ------------------------------------------------------------------------------------------------------
+// Objects :- 
+// ----------
+
+console.log(typeof undefined)
+console.log(typeof null)
